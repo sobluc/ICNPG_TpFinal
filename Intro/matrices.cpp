ec6bde4cc78e9c4a9ff7d5e925bd6a4f8a54776f
Lucas Sobehart <lucas.sobehart@ib.edu.ar>
2023-04-18 22:22:06 -0300

    Cambios en matrices.cpp

--- Intro/matrices.cpp
+++ Intro/matrices.cpp
@@ -1,3 +1,5 @@
+// Multiplicación de matrices para comparar la velocidad de corrida en cpu contra la corrida en paralelo usando cluster de GPU
+
 #include <iostream>
 
 using namespace std;
@@ -9,7 +11,7 @@ void productoMatricial(int* A, int* B, int N){
     int* C = new int [N*N];
     for(int i = 0; i < N ; i++){
         for(int j = 0; j < N ; j++){
-           int column_row_sum = 0;
+            int column_row_sum = 0;
             for(int k = 0; k < N; k++){
                 column_row_sum += A[i + N*k] * B[k + N*j];
             }
@@ -21,10 +23,11 @@ void productoMatricial(int* A, int* B, int N){
     for(int i = 0; i < N ; i++){
         for(int j = 0; j < N ; j++){
             if(j == 0) cout << "[" ;
+
+            if(j + 1 == N) cout << C[i + N*j] << "]" << endl;            
+            else  cout << C[i + N*j] << '\t';
             
-            cout << C[i + N*j] << '\t';
-            
-            if(j + 1 == N) cout << "]" << endl; 
+ 
         }
     }    
 
@@ -40,17 +43,18 @@ int main(){
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            A[i + N*j] = i + N*j; 
+            A[i + N*j] =  i + N*j; 
         }
     }  
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            B[i + N*j] = -1; 
+            if (i == j) B[i + N*j] = -1; 
+            else  B[i + N*j] = 0;
         }
     }
 
-    
+    productoMatricial(A, B, N);
 
     delete [] A;
     delete [] B;

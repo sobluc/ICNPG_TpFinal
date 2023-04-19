ed8f56c1f5a6931527188ced4a4460ed5c03ad19
Lucas Sobehart <lucas.sobehart@ib.edu.ar>
2023-04-18 22:06:32 -0300

    Cambios en matrices.cpp

--- Intro/matrices.cpp
+++ Intro/matrices.cpp
@@ -2,12 +2,33 @@
 
 using namespace std;
 
+
+
+
 void productoMatricial(int* A, int* B, int N){
+    int* C = new int [N*N];
     for(int i = 0; i < N ; i++){
         for(int j = 0; j < N ; j++){
-            cout << endl;
+           int column_row_sum = 0;
+            for(int k = 0; k < N; k++){
+                column_row_sum += A[i + N*k] * B[k + N*j];
+            }
+
+            C[i + N*j] = column_row_sum;
         }
     }
+
+    for(int i = 0; i < N ; i++){
+        for(int j = 0; j < N ; j++){
+            if(j == 0) cout << "[" ;
+            
+            cout << C[i + N*j] << '\t';
+            
+            if(j + 1 == N) cout << "]" << endl; 
+        }
+    }    
+
+    delete[] C;
 }
 
 
@@ -29,7 +50,9 @@ int main(){
         }
     }
 
-    delete [] A;
+    
 
+    delete [] A;
+    delete [] B;
     return 0;
 }
